f08c04a2bd542d83409379fb8e5acbb39556dddb
miguelalvalm <miguelalvalmeida@gmail.com>
2025-08-30 16:19:06 +0100

    minor fixes

--- config/initializers/mlg.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.tap do |config|
-  config.next_mcu_film_production_api = ENV["NEXT_MCU_FILM_PRODUCTION_API_LINK"]
-  config.next_mcu_film_dev_api = ENV["NEXT_MCU_FILM_DEV_API_LINK"]
-end
