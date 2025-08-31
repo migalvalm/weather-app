bf62df2259cbd86997a2371f2284b95d775bc5ae
miguelalvalm <miguelalvalmeida@gmail.com>
2025-08-31 23:15:44 +0100

    Add service to use sunset api if not use cache value

---
+++ app/services/fetch_date_range_sunset_sunrise.rb
@@ -0,0 +1,36 @@
+class FetchDateRangeSunsetSunrise
+  def call(latitude:, longitude:, start_date:, end_date:)
+    cached_data = find_cached_data(latitude, longitude, start_date, end_date)
+    return cached_data if cached_data
+
+    api_data = SunsetSunriseApi::Client.get_date_range(
+      latitude: latitude,
+      longitude: longitude,
+      start_date: start_date,
+      end_date: end_date
+    )
+    
+    store_data(latitude, longitude, start_date, end_date, api_data)
+  end
+
+  private
+
+  def find_cached_data(latitude, longitude, start_date, end_date)
+    HistoricalInformation.find_by(
+      latitude: latitude,
+      longitude: longitude,
+      start_date: start_date,
+      end_date: end_date
+    )
+  end
+
+  def store_data(latitude, longitude, start_date, end_date, data)
+    HistoricalInformation.create!(
+      latitude: latitude,
+      longitude: longitude,
+      start_date: start_date,
+      end_date: end_date,
+      data: data
+    )
+  end
+end
